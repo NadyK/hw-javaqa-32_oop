7f95c26e267c33c2f3e7538893c9569b6ef02bc7
Naan <naanko2021@gmail.com>
2022-01-16 17:21:55 +0300

    Initial commit

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class Post {
+    private String id;
+    private String name;
+    private String tema;
+    private String author;
+    private String wallText;
+    private String imagUrl;
+    private String dateInfo;
+    private String music;
+    private int likeInfo;
+    private String commentInfo;
+    private int viewInfo;
+    private int sendInfo;
+
+
+
+}
---
+++ src/main/java/manager/MainPagePostManager.java
@@ -0,0 +1,12 @@
+package manager;
+
+public class MainPagePostManager {
+
+    private PostManager postManager;
+
+    public PostManager getMainPostManager(){
+        postManager.getPostsForFeed();
+        //logic;
+    return null;
+    }
+}
